1307df829b0a9e9f99a25a55ece541c9602bd769
felixNyalenda <felixasejunior@gmail.com>
2024-01-21 00:08:42 +0300

    fetch upcoming events build

---
+++ src/components/index.ts
@@ -0,0 +1,17 @@
+import UpcomingEvents from "./home/upcoming-events";
+import DealsChart from "./home/deals-chart";
+import UpcomingEventsSkeleton from "./skeletons/upcoming-events";
+import AccordionHeaderSkeleton from "./skeletons/accordion-header";
+import KanbanColumnSkeleton from "./skeletons/kanban";
+import LatestActivitiesSkeleton from "./skeletons/latest-activities";
+import ProjectCardSkeleton from "./skeletons/project-card";
+
+export {
+  UpcomingEvents,
+  DealsChart,
+  UpcomingEventsSkeleton,
+  AccordionHeaderSkeleton,
+  KanbanColumnSkeleton,
+  LatestActivitiesSkeleton,
+  ProjectCardSkeleton,
+};
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,40 @@
+import { DealsChart, UpcomingEvents } from "@/components";
+import { Col, Row } from "antd";
 
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Row
+        style={{
+          border: "1px solid blue",
+          marginTop: "32px",
+        }}
+        gutter={[32, 32]}
+      >
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+          style={{
+            height: "460px",
+            border: "1px solid red",
+          }}
+        >
+          <UpcomingEvents />
+        </Col>
 
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+          style={{
+            height: "460px",
+            border: "1px solid red",
+          }}
+        >
+          <DealsChart />
+        </Col>
+      </Row>
+    </div>
+  );
+};
