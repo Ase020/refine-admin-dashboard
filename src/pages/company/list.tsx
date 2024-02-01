cac8d504c619033cdc8fb32764f2212ede0be87a
felixNyalenda <felixasejunior@gmail.com>
2024-02-01 17:33:55 +0300

    remove error

--- src/pages/company/list.tsx
+++ src/pages/company/list.tsx
@@ -8,17 +8,23 @@ import {
   List,
   useTable,
 } from "@refinedev/antd";
-import { getDefaultFilter, useGo } from "@refinedev/core";
+import { HttpError, getDefaultFilter, useGo } from "@refinedev/core";
 import { SearchOutlined } from "@ant-design/icons";
 import CustomAvatar from "@/components/custom-avatar";
 import { Text } from "@/components/text";
 import { Company } from "@/graphql/schema.types";
 import { currencyNumber } from "@/utilities";
+import { GetFieldsFromList } from "@refinedev/nestjs-query";
+import { CompaniesListQuery } from "@/graphql/types";
 
 export const CompanyList = ({ children }: React.PropsWithChildren) => {
   const go = useGo();
 
-  const { tableProps, filters } = useTable({
+  const { tableProps, filters } = useTable<
+    GetFieldsFromList<CompaniesListQuery>,
+    HttpError,
+    GetFieldsFromList<CompaniesListQuery>
+  >({
     resource: "companies",
     onSearch: (values) => [
       {
